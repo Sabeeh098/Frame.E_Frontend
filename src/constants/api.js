b2dd4d55896ea0106b081704f936be5a9edc3aea
Sabeeh098 <sabeehup@gmail.com>
2023-11-15 20:53:40 +0530

    url changed

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,11 +1,11 @@
 
-// export const UserAPI ='http://3.111.170.130/'  
-// export const artistAPI = 'http://3.111.170.130/artist' 
-// export const adminAPI = 'http://3.111.170.130/admin'
+export const UserAPI ='http://3.111.170.130'  
+export const artistAPI = 'http://3.111.170.130/artist' 
+export const adminAPI = 'http://3.111.170.130/admin'
 
-export const UserAPI =['http://localhost:4000/','http://3.111.170.130/' ]
-export const artistAPI = ['http://localhost:4000/artist' ,'http://3.111.170.130/artist']
-export const adminAPI =['http://localhost:4000/admin','http://3.111.170.130/admin']
+// export const UserAPI =['http://localhost:4000/','http://3.111.170.130/' ]
+// export const artistAPI = ['http://localhost:4000/artist' ,'http://3.111.170.130/artist']
+// export const adminAPI =['http://localhost:4000/admin','http://3.111.170.130/admin']
 
 // export const UserAPI = 'http://localhost:4000';
 // export const artistAPI = 'http://localhost:4000/artist';
