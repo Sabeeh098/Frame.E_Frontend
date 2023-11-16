b2773ae3c752fbb1309def46025aac156a78cfa8
Sabeeh098 <sabeehup@gmail.com>
2023-11-16 11:20:19 +0530

    url

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,7 +1,7 @@
 
-export const UserAPI ='http://3.111.170.130/'  
-export const artistAPI = 'http://3.111.170.130/artist/' 
-export const adminAPI = 'http://3.111.170.130/admin/' 
+export const UserAPI ='https://api.shoesdeck.online'  
+export const artistAPI = 'https://api.shoesdeck.online/artist/' 
+export const adminAPI = 'https://api.shoesdeck.online/admin/' 
 
 // export const UserAPI =['http://localhost:4000/','http://3.111.170.130/' ]
 // export const artistAPI = ['http://localhost:4000/artist' ,'http://3.111.170.130/artist']
